1bc8a8f9f490dcd7f83bb788e2b9ca9676de990e
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-03-23 20:51:28 +0100

    setup tailwind and vite config with svgr plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr(), tailwindcss()],
+  server: {
+    open: "/",
+    watch: {
+      usePolling: true,
+    },
+  },
+});
