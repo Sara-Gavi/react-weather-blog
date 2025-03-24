534ddd858f2c9057d981104d96694b5edf98cd6a
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-03-24 08:07:28 +0100

    Add complete weather endpoint and data cleaning function in Express backend

--- src/index.js
+++ src/index.js
@@ -19,3 +19,36 @@ if (process.env.NODE_ENV !== "test") {
     console.log(`Server listening at <http://localhost:${PORT}/>`);
   });
 }
+
+function clearRecord(json) {
+  return {
+    weather: json.weather[0].main,
+    weather_description: json.weather[0].description,
+    temp: json.main.temp,
+    temp_feels_like: json.main.feels_like,
+    temp_min: json.main.temp_min,
+    temp_max: json.main.temp_max,
+    pressure: json.main.pressure,
+    humidity: json.main.humidity,
+    wind_speed: json.wind.speed,
+    wind_direction: json.wind.deg,
+    clouds: json.clouds.all,
+  };
+}
+
+// ENDPOINTS
+
+app.get("/api/weather/:country/:city", async (req, res) => {
+  const city = req.params.city.toLowerCase();
+  const country = req.params.country.toLowerCase();
+
+  const API_KEY = process.env.WEATHER_API_KEY;
+
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&APPID=${API_KEY}`
+  );
+  const weatherData = await weatherResponse.json();
+  const weatherRecord = clearRecord(weatherData);
+
+  return res.json(weatherRecord);
+});
